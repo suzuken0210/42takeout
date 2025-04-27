d3a3b4a2536a4a7487e3b8828f90927d46bc7541
Kenta Suzuki <kesuzuki@c1r3s2.42tokyo.jp>
2025-04-27 17:54:51 +0900

    memcmpを追加

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,4 +1,37 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main
+int ft_memcmp(const char *s, const char *t, int d)
+{
+    int i;
+    i = 0;
+    while(i != d)
+    {
+        if(s[i] != t[i])
+        {
+            return(1);
+            break;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return(0);
+}
+
+int main(void)
+{
+    char *s;
+    s = "abcde";
+    char *t;
+    t = "abcfe";
+    int d;
+    d = 3;
+    int p;
+    p = ft_memcmp(s,t,d);
+    write(1, "-", 1);
+    char char_p = p + '0';
+    write(1, &char_p, 1);
+    return(0);
+}
